ce3b38734385af40c6ce3752ed89ddb31ef98b88
Fabibourdokan <fabianabourdokanvaliente@gmail.com>
2022-07-07 19:47:24 -0300

    desafio entregue

---
+++ script.js
@@ -0,0 +1,10 @@
+function funcao() {console.log('Olá Mundo!')}
+funcao()
+
+function seila(oii) {console.log(`Oi ${oii}`)}
+seila('saudação')
+
+function informacao(nome, idade, musica) {console.log(`O meu nome é ${nome}, minha idade ${idade}, meu gosto musical ${musica}`)}
+
+function informacaoDois(filme, musica) {console.log(`Um filme:${filme}, uma musica:${musica}`)}
+informacaoDois('Simplesmente acontece', 'Nada contra')
